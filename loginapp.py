6721c37b0961f7a63866a9e93d1a90fe3cf912b2
Shakeer1024 <37765984+Shakeer1024@users.noreply.github.com>
2020-05-21 23:52:21 +0530

    Update loginapp.py

--- loginapp.py
+++ loginapp.py
@@ -12,6 +12,7 @@ app =Flask(__name__)
 # Configure db
 #db = yaml.load(open('db.yaml'))
 app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'loginapp'
