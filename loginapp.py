51f9f846e1b395b0a33be4cf83cb6321e145663d
Shakeer1024 <37765984+Shakeer1024@users.noreply.github.com>
2020-05-31 13:55:10 +0530

    Add files via upload

---
+++ loginapp.py
@@ -0,0 +1,120 @@
+import flask
+from flask import Flask,session,request,render_template,redirect,url_for,flash
+from flask_mysqldb import MySQL
+
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import scoped_session,sessionmaker
+
+#from flaskext.mysql import MySQL
+#import yaml
+
+app =Flask(__name__)
+
+# Configure db
+#db = yaml.load(open('db.yaml'))
+#app.config['MYSQL_HOST'] = '127.0.0.1'
+#app.config['MYSQL_PORT'] = 3306
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = 'root'
+#app.config['MYSQL_DB'] = 'loginapp'
+#app.config['MYSQL_CURSORCLASS']='DictCursor'
+#app.config['SECRET_KEY'] = "b'\x8e\x88}\xd9hC\\6z:,$'"
+
+#app.config['MYSQL_HOST'] = db['mysql_host']
+#app.config['MYSQL_PORT'] =db['mysql_port']
+#app.config['MYSQL_USER'] = db['mysql_user']
+#app.config['MYSQL_PASSWORD'] = db['mysql_password']
+#app.config['MYSQL_DB'] = db['mysql_db']
+#app.config['MYSQL_CURSORCLASS'] =db['mysql_cursorclass']
+#app.config['SECRET_KEY'] =db['secret_key']
+
+app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'b81ff3091705f9'
+app.config['MYSQL_PASSWORD'] = 'd3f20e8c'
+app.config['MYSQL_DB'] = 'heroku_8178ef1d9ae0bf2'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+app.config['SECRET_KEY'] = "b'\x8e\x88}\xd9hC\\6z:,$'"
+
+mysql = MySQL(app)
+
+
+
+@app.route('/')
+def index():
+	return render_template('login.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+	if request.method == "POST":
+		Name=request.form.get("name")
+		UserName=request.form.get("username")
+		Password=request.form.get("password")
+		confirm=request.form.get("confirm password")
+		cur = mysql.connection.cursor()
+		cur.execute("SELECT UserName FROM users WHERE UserName='"+ UserName +"'")
+		userdata=cur.fetchone()
+
+		
+
+		if userdata is None:
+			if Password == confirm:
+				cur = mysql.connection.cursor()
+				cur.execute("INSERT INTO users(Name,UserName,Password) VALUES(%s,%s,%s)",(Name,UserName,Password))
+				mysql.connection.commit()
+
+				session['name']=Name
+
+				flash("Registered Successfully and you can login now",'success')
+				return redirect(url_for('index'))
+			else:
+				flash("Password is incorrect",'error')
+				return render_template('register.html')
+		else:
+			flash('username already exist! please user different user name','error')
+			return render_template('register.html')
+
+	return render_template('register.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	if request.method=='POST':
+		session.pop('user',None)
+		UserName=request.form.get("username")
+		Password=request.form.get("password")
+
+		cur = mysql.connection.cursor()
+		cur.execute("SELECT UserName FROM users WHERE UserName='"+ UserName +"'")
+		userdata=cur.fetchone()
+		cur.execute("SELECT Password FROM users WHERE UserName='"+ UserName +"'")
+		pwddata=cur.fetchone()
+		session['user']=UserName
+
+		if userdata is None:
+			flash("No such user found, Please register",'error')
+			return redirect(url_for('index'))
+		else:
+			if Password==pwddata['Password']:
+				flash("Loggedin Successfully", 'success')
+				return redirect(url_for('login'))
+			else:
+				flash("Incorrect username or password",'error')
+				return redirect(url_for('index'))
+
+	return render_template('loginhome.html')
+
+@app.route('/chatbot')
+def chatbot():
+	return render_template('chatbot.html')
+
+
+@app.route('/logout')
+def logout():
+	session.pop('user',None)
+	flash("Your are now logged out",'success')
+	return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
